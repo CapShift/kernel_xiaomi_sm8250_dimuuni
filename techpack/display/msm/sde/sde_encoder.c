25b2aba9680208ac9e9bbf2c772beb21067abf5a
Danny Lin <danny@kdrag0n.dev>
2024-02-29 14:40:58 +0800

    display: Trigger panel reset after first kickoff If we're using a refresh rate different from what the bootloader initializes the display with, a panel reset is necessary for our custom refresh rate to take effect.
    After the first kickoff, request a panel reset from userspace using the
    existing ESD recovery mechanism to update the refresh rate.
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Change-Id: I2a4dcae2043d651926929aa422ab34d655c15991
    
    [RealJohnGalt]: adapt to k4.14. Fixes rare case of locked wrong rate gamma
    on boot.

--- techpack/display/msm/sde/sde_encoder.c
+++ techpack/display/msm/sde/sde_encoder.c
@@ -5135,6 +5135,7 @@ static int _sde_encoder_reset_ctl_hw(struct drm_encoder *drm_enc)
 
 void sde_encoder_kickoff(struct drm_encoder *drm_enc, bool is_error)
 {
+	static bool first_run = true;
 	struct sde_encoder_virt *sde_enc;
 	struct sde_encoder_phys *phys;
 	struct dsi_bridge *bridge = NULL;
@@ -5181,6 +5182,21 @@ void sde_encoder_kickoff(struct drm_encoder *drm_enc, bool is_error)
 		phys = sde_enc->phys_encs[i];
 		if (phys && phys->ops.handle_post_kickoff)
 			phys->ops.handle_post_kickoff(phys);
+	}
+
+	/* Trigger a panel reset if this is the first kickoff */
+	if (cmpxchg(&first_run, true, false)) {
+		struct sde_connector *conn = container_of(phys->connector, struct sde_connector, base);
+		struct drm_event event = {
+			.type = DRM_EVENT_PANEL_DEAD,
+			.length = sizeof(bool)
+		};
+
+		conn->panel_dead = true;
+		event.type = DRM_EVENT_PANEL_DEAD;
+		event.length = sizeof(bool);
+		msm_mode_object_event_notify(&conn->base.base,
+			conn->base.dev, &event, (u8 *) &conn->panel_dead);
 	}
 
 	if (sde_enc->disp_info.intf_type == DRM_MODE_CONNECTOR_DSI &&
